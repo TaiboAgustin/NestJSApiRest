9dcddce42d26e92d4a38fd1ad0a65209349e6113
Agustin Taibo <taibocru@gmail.com>
2023-12-28 11:11:18 -0300

    finished Api rest

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,49 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Delete, Put, NotFoundException } from "@nestjs/common";
+import { TaskService } from "./task.service";
+import { Task } from "@prisma/client";
+import { NotFoundError } from "rxjs";
 
+@Controller('tasks')
+export class TaskController {
+    constructor(private readonly taskService: TaskService) {}
 
+    @Get()
+    async getAllTasks() {
+        return this.taskService.getAllTasks();
+    }
 
-@Controller()
-export class TaskController{}
+    @Post()
+    async createTask(@Body() data: Task){
+        return this.taskService.createTask(data);
+    }
+
+    @Get(':id')
+    async getTaskById( @Param( 'id' ) id:string ) {
+        const taskFound = await this.taskService.getTaskById(Number(id))
+
+        if (!taskFound) throw new NotFoundException("La tarea con el id solicitado no existe")
+        return taskFound 
+    }
+
+    @Delete(':id')
+    async deleteTask( @Param( 'id' ) id:string ) {
+        try{
+            return await this.taskService.deleteTask(Number(id))
+        }
+        catch(error){
+            throw new NotFoundException("La tarea no existe, asi que no se puede eliminar nada")
+        }
+    }
+
+    @Put(':id')
+    async updateTask( @Param( 'id' ) id:string, @Body() data:Task ) {
+        try{
+            return await this.taskService.updateTask(Number(id), data)
+        }
+        catch(error){
+            throw new NotFoundException("La tarea no existe, asi que no se puede actualizar nada")
+        }
+
+    }
+    
+}
